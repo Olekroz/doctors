0ea0ebc9a4c55c1afa7057a3eeebf1b7b847301a
Anna <anna.wisniowska@onwelo.com>
2023-01-11 15:11:39 +0100

    first HTML changes

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,6 +1,18 @@
 <template>
-  <div class="bg-red-500">
-    Hello World
+  <div class="bg-blue-200">
+    <div>
+      Sign in to your account
+      <div>
+        <form class="bg-white">
+          <label for="email">Email address</label>
+          <input type="text" name="email" id="email">
+          <label for="password">Password</label>
+          <input type="password" name="password" id="password">
+          <p>Forgot your password?</p>
+          <button>Sign in</button>
+        </form>
+      </div>
+    </div>
   </div>
 </template>
 
