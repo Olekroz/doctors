ca9df675552860b05ba0b71f59d0fd29cb8a21a8
Anna <anna.wisniowska@onwelo.com>
2023-01-12 16:56:35 +0100

    styling padding changes

--- src/components/SignIn.vue
+++ src/components/SignIn.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="bg-blue-200 h-screen font-nunito">
+  <div class="bg-blue-100 h-screen font-nunito">
      <div class="flex justify-center py-12">
       <div class="space-y-8 w-[500px]">
 
@@ -10,10 +10,10 @@
             </svg>
 
           </span>
-          <h1 class="text-center ">Sign in to your account</h1>
+          <h1 class="text-center font-bold">Sign in to your account</h1>
         </div>
       
-        <div class="h-full w-full py-4 bg-white rounded-xl px-8">
+        <div class="h-auto w-full py-8 bg-white rounded-xl px-10">
           <form class="bg-white flex-column py-3 space-y-6">
             <div>
               <label for="email" class="block font-bold">Email address</label>
